dfc56afcd45b10a4b8d722021c110355d0d19149
RohanSha05 <rhnshariar@gmail.com>
2022-05-09 16:31:45 +0600

    passwordReset

--- src/Pages/Login/GoogleLogin/GoogleLogin.js
+++ src/Pages/Login/GoogleLogin/GoogleLogin.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from '../../../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 
 const googleProvider = new GoogleAuthProvider();
 
 const GoogleLogin = () => {
-    /*  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
- 
-     const signInWithGoogle = () => {
-         signInWithPopup(auth, googleProvider)
-             .then(result => {
-                 const user = result.user
-             })
-     } */
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+
+    let errorElement;
+    if (error) {
+        return (
+
+            errorElement = <div>
+                <p className='text-danger text-center'>Error: {error.message}</p>
+            </div>
+        );
+    }
+
+    if (user) {
+        navigate('/')
+    }
+
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -21,8 +31,11 @@ const GoogleLogin = () => {
                 <div className='px-3'>or</div>
                 <div className='bg-success w-50' style={{ height: '1px' }}></div>
             </div>
-            <div>
-                <button className='btn btn-dark w-50'>Google Sign In</button>
+            {errorElement}
+            <div className='d-flex justify-content-center'>
+                <button
+                    onClick={() => signInWithGoogle()}
+                    className='btn btn-dark w-25'>Google Sign In</button>
             </div>
         </div>
     );
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,12 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import GoogleLogin from '../GoogleLogin/GoogleLogin';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -36,12 +39,20 @@ const Login = () => {
     const navigateRegister = event => {
         navigate('/register')
     }
+    const [sendPasswordResetEmail, sending, error1] = useSendPasswordResetEmail(
+        auth
+    );
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        await sendPasswordResetEmail(email);
+        toast('Email Sent')
+    }
 
 
     return (
         <div>
-            <h3>Please Login</h3>
-            <Form onSubmit={handleSubmit} className='w-50 mx-auto m-5'>
+            <h3 className='text-center mt-5'>Please Login</h3>
+            <Form onSubmit={handleSubmit} className='w-50 mx-auto mt-0 m-5'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
@@ -51,13 +62,16 @@ const Login = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control ref={passRef} type="password" placeholder="Password" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
+                <Button variant="dark" type="submit">
+                    Login
                 </Button>
             </Form>
-            <p className='text-center'>Not registered yet? <Link onClick={navigateRegister} to="/register">Register</Link></p>
+
+            <p className='text-center'>Not registered yet? <Button onClick={navigateRegister} variant="dark" to="/register">Register</Button></p>
+            <p className='text-center'>Forget Password? <Button variant="dark" onClick={resetPassword}>Reset Password</Button></p>
 
             <GoogleLogin></GoogleLogin>
+            <ToastContainer></ToastContainer>
 
         </div>
     );
